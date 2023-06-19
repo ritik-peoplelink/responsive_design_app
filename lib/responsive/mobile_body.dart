87b2fc58c71ec5733a619437da429b1686534e02
The-BigMouse <ritikbhushanyadav@gmail.com>
2023-06-19 19:06:08 +0530

    first commit

---
+++ lib/responsive/mobile_body.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class MyMobileBody extends StatelessWidget {
+  const MyMobileBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple[200],
+      appBar: AppBar(
+        title: const Text('M O B I L E V I E W'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: AspectRatio(
+                aspectRatio: 16 / 9,
+                child: Container(
+                  color: Colors.deepPurple[400],
+                ),
+              ),
+            ),
+
+            Expanded(
+              child: ListView.builder(
+                itemCount: 8,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      color: Colors.deepPurple[300],
+                      height: 120,
+                    ),
+                  );
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
